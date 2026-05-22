47139b0d55847e67ea79e371d56a8f49f9160249
manojnambiraj <postboxmano@gmail.com>
2026-05-22 19:51:34 +0530

    Methods

---
+++ src/main/java/org/example/class6/MyArrays.java
@@ -0,0 +1,31 @@
+package org.example.class6;
+
+public class MyArrays {
+    static void main(String[] args) {
+//        Arrays
+
+//        int[] arr = {1, 2, 3, 4, 5};
+//                     #0  #1 #2.......
+
+//        String[] arr = {"Volvo", "BMW", "Hyundai"};
+
+        String[] arr = new String[3];
+
+        arr[0] = "Ferrari";
+        arr[1] = "Ford";
+        arr[2] = "Audi";
+
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr.length);
+
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+
+        for(String item : arr){
+            System.out.println(item);
+        }
+    }
+}
---
+++ src/main/java/org/example/class7/MyMethods.java
@@ -0,0 +1,13 @@
+package org.example.class7;
+
+public class MyMethods {
+
+    static void display(int x){                // Method definition
+        System.out.println(x * 2);
+    }
+
+    static void main(String[] args) {
+        display(150);                        // Method call
+        display(2700);                        // Method call
+    }
+}
