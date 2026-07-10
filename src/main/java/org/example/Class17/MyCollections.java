e0c6c44d750ffe251f648ff3d442bb9b1e46b75f
manojnambiraj <postboxmano@gmail.com>
2026-07-10 20:57:03 +0530

    Java Collections - Arraylist

---
+++ src/main/java/org/example/Class17/MyCollections.java
@@ -0,0 +1,59 @@
+package org.example.Class17;
+
+// Collections API's
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class MyCollections {
+    static void main(String[] args) {
+//        ArrayList
+
+//        ArrayList<String> names = new ArrayList<>();
+//
+//        names.add("John");
+//        names.add("Arun");
+//        names.add("Rahul");
+//
+//        System.out.println(names);
+
+//        names.remove("Rahul");
+
+//        System.out.println(names);
+//        System.out.println(names.size());
+//        System.out.println(names.get(0));
+
+//    for(int i = 0; i < names.size(); i++){
+//        System.out.println(names.get(i));
+//    }
+
+//        for(String name : names) {
+//            System.out.println(name);
+//        }
+
+//        LinkedList
+
+//        LinkedList<String> cities = new LinkedList<>();
+//
+//        cities.add("London");
+//        cities.add("New York");
+//
+//        cities.addFirst("UK");
+//        cities.addLast("Arab");
+//
+//        System.out.println(cities);
+//
+//        cities.removeFirst();
+//
+//        System.out.println(cities);
+
+//        Vector<Integer> v = new Vector<>();
+//
+//        v.add(1);
+//        v.add(100);
+//        v.add(1200);
+//
+//        System.out.println(v);
+    }
+}
